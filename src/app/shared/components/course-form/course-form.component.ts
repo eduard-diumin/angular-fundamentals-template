a14f73f8382afeb2ee845fa0d93c56c081da3c52
Eduard Diumin <edwarddumin@gmail.com>
2025-09-21 22:10:12 +0300

    feature: add task03 tasks

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,19 +1,95 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import {
-  FormBuilder, FormGroup
-} from '@angular/forms';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+interface Author {
+  id: string;
+  name: string;
+}
 
 @Component({
-  selector: 'app-course-form',
-  templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss'],
+  selector: "app-course-form",
+  templateUrl: "./course-form.component.html",
+  styleUrls: ["./course-form.component.scss"],
 })
 export class CourseFormComponent {
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
   courseForm!: FormGroup;
+  submitted = false;
+
+  allAuthors: Author[] = [];
+  courseAuthors: Author[] = [];
   // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
+  ngOnInit(): void {
+    this.courseForm = this.fb.group({
+      title: ["", [Validators.required, Validators.minLength(2)]],
+      description: ["", [Validators.required, Validators.minLength(2)]],
+      duration: [0, [Validators.required, Validators.min(0)]],
+      authors: this.fb.array([]), // Для авторов курса
+      newAuthor: this.fb.group({
+        name: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.pattern("^[a-zA-Z0-9]+$"),
+          ],
+        ],
+      }),
+    });
+
+    // Mock data for authors
+    this.allAuthors = [
+      { id: "1", name: "JohnDoe" },
+      { id: "2", name: "JaneSmith" },
+    ];
+  }
+
+  get authors(): FormArray {
+    return this.courseForm.get("authors") as FormArray;
+  }
+
+  get newAuthorGroup(): FormGroup {
+    return this.courseForm.get("newAuthor") as FormGroup;
+  }
+
+  addAuthorToCourse(author: Author) {
+    this.courseAuthors.push(author);
+    this.authors.push(new FormControl(author));
+    this.allAuthors = this.allAuthors.filter((a) => a.id !== author.id);
+  }
+
+  removeAuthorFromCourse(author: Author, index: number) {
+    this.allAuthors.push(author);
+    this.courseAuthors = this.courseAuthors.filter((a) => a.id !== author.id);
+    this.authors.removeAt(index);
+  }
+
+  createAuthor() {
+    const name = this.newAuthorGroup.get("name")?.value;
+    if (this.newAuthorGroup.valid) {
+      const newAuthor: Author = {
+        id: Math.random().toString(36).substring(2, 10),
+        name,
+      };
+      this.allAuthors.push(newAuthor);
+      this.newAuthorGroup.reset();
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.courseForm.valid && this.courseAuthors.length > 0) {
+      console.log(this.courseForm.value);
+    }
+  }
 }
--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss'],
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.component.html",
+  styleUrls: ["./registration-form.component.scss"],
 })
 export class RegistrationFormComponent {
   registrationForm!: FormGroup;
   // Use the names `name`, `email`, `password` for the form controls.
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      email: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+    });
+  }
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    }
+  }
 }
